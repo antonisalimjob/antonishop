edcb61019dfd4b6da8a1375bc3afdacdf4838fd1
antonisalimjob <antoni.salim.job@gmail.com>
2026-09-02 08:07:12 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,13 @@
 'use client' // WAJIB ada di baris pertama file
 
-import { createBrowserClient } from '@supabase/ssr'
-
-export default function LoginPage() {
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-
-  const handleGoogleLogin = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${window.location.origin}/shop/auth/callback`,
-      },
-    })
-  }
+const handleGoogleLogin = async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${window.location.origin}/shop/auth/callback`,
+    },
+  })
+}
 
   return (
     <button
