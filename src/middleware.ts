37765e5f011c9f96bd6fe93c80bc61041d62265e
antonisalimjob <antoni.salim.job@gmail.com>
2026-09-02 08:12:32 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,7 @@ export async function middleware(request: NextRequest) {
               ...options,
               path: '/',
               sameSite: 'lax',
-              secure: process.env.NODE_ENV === 'production',
+              secure: true,
             })
           )
         },
@@ -38,6 +38,7 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  // Hanya redirect jika mencoba akses /account tanpa session
   if (!user && request.nextUrl.pathname.includes('/account')) {
     const url = request.nextUrl.clone()
     url.pathname = '/shop/login'
